5c501f126307ad81d8f9b1658edc2ac614ced921
Bruno Basto <bruno.basto@liferay.com>
2019-03-09 12:56:19 -0300

    Adds build files to example

---
+++ packages/pebble-react-example/src/App.js
@@ -0,0 +1,120 @@
+import React, { Component, Fragment } from 'react';
+
+const letterWidth = 29;
+
+class App extends Component {
+    state = {
+        what: 'React'
+    };
+
+    componentDidMount() {
+        Pebble.addEventListener(
+            'appmessage',
+            event => {
+                this.setState(
+                    {
+                        what: 'Pebble'
+                    }
+                );
+            }
+        );
+    }
+
+    render() {
+        const { what } = this.state;
+
+        return (
+            <Fragment>
+                <arc
+                    top={55}
+                    left={70}
+                    radius={50}
+                    startAngle={-90}
+                    endAngle={90}
+                />
+
+                <animation
+                    animationProps={{
+                        left: {
+                            start: -30,
+                            end: 0
+                        },
+                        top: {
+                            start: -30,
+                            end: 0
+                        }
+                    }}
+                    duration={750}
+                >
+                    <layer
+                        height={30}
+                        top={-30}
+                        width={144}
+                    >
+                        <animation
+                            animationProps={{
+                                radius: {
+                                    start: 0,
+                                    end: 7
+                                }
+                            }}
+                            delay={750}
+                            duration={500}
+                        >
+                            <circle top={13} left={15} />
+                        </animation>
+
+                        <animation
+                            animationProps={{
+                                color: {
+                                    start: "#5F98E1",
+                                    end: "#073CAA"
+                                }
+                            }}
+                            delay={750}
+                            duration={500}
+                        >
+                            <text
+                                alignment={'left'}
+                                color={"#5F98E1"}
+                                height={30}
+                                left={30}
+                                width={10 * what.length}
+                            >
+                                {what}
+                            </text>
+                        </animation>
+                    </layer>
+                </animation>
+
+                {'Rocks'.split('').map((letter, index) => (
+                    <animation
+                        animationProps={{
+                            color: {
+                                start: "#5F98E1",
+                                end: "#CCCB40"
+                            },
+                            left: {
+                                start: index % 2 == 0 ? letterWidth * -1 : 200,
+                                end: (index * letterWidth)
+                            }
+                        }}
+                        duration={2000}
+                    >
+                        <text
+                            color="#5F98E1"
+                            height={30}
+                            left={index % 2 == 0 ? letterWidth * -1 : 200}
+                            top={65}
+                            width={letterWidth}
+                        >
+                            {letter}
+                        </text>
+                    </animation>
+                ))}
+            </Fragment>
+        );
+    }
+}
+
+export default App;
---
+++ packages/pebble-react-example/webpack.config.js
@@ -0,0 +1,41 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+function resolveModule(name) {
+  return path.resolve(__dirname, `src/${name}`);
+}
+
+const config = {
+  entry: [
+    resolveModule('index.js')
+  ],
+  module: {
+    loaders: [
+      {
+        exclude: [
+          /node_modules/
+        ],
+        include: __dirname,
+        loaders: ['babel-loader'],
+      },
+      {
+        loaders: ['json-loader'],
+        test: /\.json$/
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('build/js')
+  },
+  plugins: [
+    new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
+    new CleanWebpackPlugin(),
+    new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
+    // new BundleAnalyzerPlugin()
+  ]
+};
+
+module.exports = config;
